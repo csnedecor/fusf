f3536695ff4135ac6c5c76e5389b8b1aa2717a0c
csnedecor <csnedecor@worthwild.com>
2016-08-01 15:09:29 -0400

    Enqueues styles in correct order

--- functions.php
+++ functions.php
@@ -1,7 +1,9 @@
 <?php
 // Enqueue Scripts
 function fusf_enqueue_styles() {
-    wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
+  wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
+  wp_enqueue_style( 'font-awesome', get_template_directory_uri() . '/css/font-awesome.min.css', '', '2.0');
+  wp_enqueue_style( 'jquery-flexslider', get_template_directory_uri() . '/css/flexslider.css', '', '2.0');
 }
 add_action( 'wp_enqueue_scripts', 'fusf_enqueue_styles' );
 
