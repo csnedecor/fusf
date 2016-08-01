4d6b1c32253bffec375097c7b79c85b07f00e0d7
csnedecor <csnedecor@worthwild.com>
2016-08-01 18:18:02 -0400

    Makes alerts page full width

--- page-templates/alerts.php
+++ page-templates/alerts.php
@@ -6,39 +6,41 @@
 
 get_header(); ?>
 
-    <div id="primary" class="content-area">
-      <div id="content" class="site-content" role="main">
+    <div id="primary_home" class="content-area">
+      <div id="content" class="fullwidth" role="main">
 
         <?php while ( have_posts() ) : the_post(); ?>
 
           <?php get_template_part( 'content', 'page' ); ?>
 
           <?php if( have_rows('alerts') ): ?>
-            <?php while(have_rows('alerts')) : the_row();
+            <div class="alerts">
+              <?php while(have_rows('alerts')) : the_row();
 
-              // vars
-              $title = get_sub_field('alert_title');
-              $description = get_sub_field('alert_description');
-              $link = get_sub_field('alert_link');
-              $level = get_sub_field('alert_level');
-              ?>
+                // vars
+                $title = get_sub_field('alert_title');
+                $description = get_sub_field('alert_description');
+                $link = get_sub_field('alert_link');
+                $level = get_sub_field('alert_level');
+                ?>
 
-              <div class="alert">
-                <?php if($level == 'high') { ?>
-                  <h2 class="alert-title red"><i class="fa fa-exclamation-circle"></i>
-                <?php } elseif($level == 'medium') { ?>
-                  <h2 class="alert-title orange"><i class="fa fa-exclamation-triangle"></i>
-                <?php } else { ?>
-                  <h2 class="alert-title green"><i class="fa fa-info"></i>
-                <?php } ?>
-                <?php echo $title ?>
-                  </h2><!-- /.alert-title-->
-                <div class="alert-caption"><?php echo $description ?></div><!-- /.alert-caption -->
-                <?php if(!empty($link)) { ?>
-                  <div class="read-more"><a href="<?php echo $link ?>">More info...</a></div><!-- /.read-more -->
-                <?php } ?>
-              </div>
-            <?php endwhile; ?>
+                <div class="alert">
+                  <?php if($level == 'high') { ?>
+                    <h2 class="alert-title red"><i class="fa fa-exclamation-circle"></i>
+                  <?php } elseif($level == 'medium') { ?>
+                    <h2 class="alert-title orange"><i class="fa fa-exclamation-triangle"></i>
+                  <?php } else { ?>
+                    <h2 class="alert-title green"><i class="fa fa-info"></i>
+                  <?php } ?>
+                  <?php echo $title ?>
+                    </h2><!-- /.alert-title-->
+                  <div class="alert-caption"><?php echo $description ?></div><!-- /.alert-caption -->
+                  <?php if(!empty($link)) { ?>
+                    <div class="read-more"><a href="<?php echo $link ?>">More info...</a></div><!-- /.read-more -->
+                  <?php } ?>
+                </div>
+              <?php endwhile; ?>
+            </div>
           <?php endif; ?>
 
           <?php comments_template( '', true ); ?>
@@ -49,5 +51,4 @@ get_header(); ?>
       </div><!-- #content .site-content -->
     </div><!-- #primary .content-area -->
 
-<?php get_sidebar(); ?>
 <?php get_footer(); ?>
